7c0f333880c6226fa7fd247cef1f57e08275c84c
BlazingNeutron <135986741+BlazingNeutron@users.noreply.github.com>
2025-09-25 19:41:13 +0000

    Env variable improvements

--- apps/web/src/quill.js
+++ apps/web/src/quill.js
@@ -10,7 +10,11 @@ import { SupabaseProvider } from './y-supabase-provider.js'
 Quill.register('modules/cursors', QuillCursors)
 
 window.addEventListener('load', () => {
-  const supabase = createClient("http://localhost:8000", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE");
+  let baseUrl = window.origin;
+  if (baseUrl.indexOf("localhost:5173") >= 0) {
+    baseUrl = "http://localhost:8000"
+  }
+  const supabase = createClient(baseUrl, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE");
   const ydoc = new Y.Doc()
   const supaProvider = new SupabaseProvider('my-shared-doc-id', ydoc, supabase);
   const ytext = ydoc.getText('quill')
