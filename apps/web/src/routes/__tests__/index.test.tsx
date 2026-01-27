8a240ea9e5d9e39c10c147e9e3a15f4a163144e2
BlazingNeutron <135986741+BlazingNeutron@users.noreply.github.com>
2026-01-27 01:01:50 +0000

    Used react act to properly render in tests

--- apps/web/src/routes/__tests__/index.test.tsx
+++ apps/web/src/routes/__tests__/index.test.tsx
@@ -1,24 +1,39 @@
-import { render } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { MemoryRouter } from 'react-router';
 import Index from '../index';
 import ThemeProvider from '../../components/ThemeProvider';
+import { act } from 'react';
+import ReactDOMClient from 'react-dom/client';
 
-vi.mock('../../lib/auth/auth-context', () => ({
+vi.mock('../../lib/auth/authContext', () => ({
   useAuth: () => ({
     session: { user: { id: 'u-123', email: '<EMAIL>' } }
   }),
 }));
 
 describe('Index component', () => {
+  let container: any;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
   it('Loads quill editor within index component', async () => {
-    const { container } = render(
-      <MemoryRouter>
-        <ThemeProvider>
-          <Index />
-        </ThemeProvider>
-      </MemoryRouter>
-    );
+    await act(async () => {
+      ReactDOMClient.createRoot(container).render(
+        <MemoryRouter>
+          <ThemeProvider>
+            <Index />
+          </ThemeProvider>
+        </MemoryRouter>
+      );
+    })
 
     const editorElement = container.querySelector('.ql-editor');
     expect(editorElement).toBeInTheDocument();
