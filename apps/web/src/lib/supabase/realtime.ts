cf32b0337d29d52c3c0df0e15b0c24233e003e8a
BlazingNeutron <135986741+BlazingNeutron@users.noreply.github.com>
2026-01-31 22:53:54 +0000

    Realtime awareness

--- apps/web/src/lib/supabase/realtime.ts
+++ apps/web/src/lib/supabase/realtime.ts
@@ -2,7 +2,8 @@ import { supabase } from "./client";
 import { Buffer } from 'buffer';
 
 export async function broadcast(docId: string, base64Update: string) {
-    await supabase.channel(`yjs-${docId}`).send({
+    const channel = await supabase.channel(`yjs-${docId}`);
+    await channel.send({
         type: 'broadcast',
         event: 'y-update',
         payload: { update: base64Update },
@@ -10,9 +11,27 @@ export async function broadcast(docId: string, base64Update: string) {
 }
 
 export async function subscribe(docId: string, callback: Function) {
-    supabase.channel(`yjs-${docId}`)
+    const channel = supabase.channel(`yjs-${docId}`)
         .on('broadcast', { event: 'y-update' }, (payload: { payload: { update: string } }) => {
             const update = Buffer.from(payload.payload.update, 'base64');
+            callback(update)
+        })
+        .subscribe();
+    return channel;
+}
+
+export async function broadcastAwareness(docId: string, base64Awareness: string) {
+    await supabase.channel(`yjs-${docId}`).send({
+        type: 'broadcast',
+        event: 'awareness',
+        payload: { awareness: base64Awareness },
+    });
+}
+
+export async function subscribeAwareness(docId: string, callback: Function) {
+    supabase.channel(`yjs-${docId}`)
+        .on('broadcast', { event: 'awareness' }, (payload: { payload: { awareness: string } }) => {
+            const update = Buffer.from(payload.payload.awareness, 'base64');
             callback(update)
         })
         .subscribe();
