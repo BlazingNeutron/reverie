584d165e5bd81e1d22bb71e9c29d72a204ad66c8
BlazingNeutron <135986741+BlazingNeutron@users.noreply.github.com>
2026-02-14 12:33:34 +0000

    Docker compose and Dockerfile for api image

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,17 +1,12 @@
 import express from "express";
 import userRouter from "./user/routes";
+import healthRouter from "./health/health";
 
 const app = express();
 
 app.use(express.json());
+app.use("/", healthRouter);
 app.use("/", userRouter);
-
-app.get("/status", (_req, res) => {
-  res.json({
-    status: "Running",
-    timestamp: new Date().toISOString(),
-  });
-});
 
 const PORT = process.env.REVERIE_API_PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
