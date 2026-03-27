35851efbb6de7389ac5b3c0fa1c35f45ff4c501e
BlazingNeutron <135986741+BlazingNeutron@users.noreply.github.com>
2026-03-27 02:46:42 +0000

    Unit tests and mock for keys

--- apps/web/src/setupTests.ts
+++ apps/web/src/setupTests.ts
@@ -2,12 +2,14 @@ import "@testing-library/jest-dom";
 import { vi } from "vitest";
 import { supabaseClientMock } from "./__mocks__/supabaseClientMock";
 import logger from "./__mocks__/loggerMock";
+import { getPublishableKey } from "./__mocks__/keysMock";
 
 // TODO later with store context vi.mock('zustand')
 vi.stubGlobal("IS_REACT_ACT_ENVIRONMENT", true);
 
 vi.mock("./lib/supabase/client", () => ({ supabase: supabaseClientMock }));
 vi.mock("./lib/logger/logger", () => ({ default: logger }));
+vi.mock("./lib/supabase/keys", () => ({ default: getPublishableKey }));
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
