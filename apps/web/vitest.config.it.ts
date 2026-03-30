229ea2ef951d128e2909ebf6f5ec2472cd0c3fb7
BlazingNeutron <135986741+BlazingNeutron@users.noreply.github.com>
2026-03-30 18:13:48 +0000

    Can run the IT tests inside devcontainer

--- apps/web/vitest.config.it.ts
+++ apps/web/vitest.config.it.ts
@@ -1,12 +1,23 @@
 import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig(({ mode }) => ({
-  test: {
-    environment: "jsdom",
-    globals: true,
-    include: ["src/**/*.it.{test,spec}.{ts,tsx}"],
-    env: loadEnv("it", process.cwd(), ""),
-    setupFiles: "src/setupTests.it.ts",
-  },
-}));
+export default defineConfig(({ mode }) => {
+  //TODO look at which server is running and maybe set SITE_URL
+  // inside devcontainer running dev - localhost:5173
+  // inside devcontainer running api on host - 172.17.0.1:3000
+  // outside container during integration tests - localhost:3000
+  const isDevContainer =
+    process.env.CODESPACES === "true" || process.env.DEVCONTAINER !== undefined;
+
+  const envMode = isDevContainer ? "dev" : "it";
+
+  return {
+    test: {
+      environment: "jsdom",
+      globals: true,
+      include: ["src/**/*.it.{test,spec}.{ts,tsx}"],
+      env: loadEnv(envMode, process.cwd(), ""),
+      setupFiles: "src/setupTests.it.ts",
+    },
+  };
+});
